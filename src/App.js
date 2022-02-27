3d416f83043468dc38368a323f7f9cb177043d35
Inhwan Wee <10ezenfinani@gmail.com>
2022-02-28 00:37:07 +0900

    build the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a LinkedIn CLONE</h1>
-
-      {/* Header */}
+      <Header />
 
       {/* App Body */}
         {/* Sidebar */}
